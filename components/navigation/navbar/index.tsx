315776dd9e81e3d5b6dd65635b08028ee2a90d8f
Imaginbright <brightified2004@gmail.com>
2025-08-11 12:55:10 +0100

    Implemented Theme Switching

---
+++ components/navigation/navbar/index.tsx
@@ -0,0 +1,32 @@
+import Image from "next/image";
+import Link from "next/link";
+import React from "react";
+import Theme from "./Theme";
+
+const Navbar = () => {
+  return (
+    <nav className="flex-between background-light900_dark200 fixed z-50 w-full p-6 shadow-light-300 dark:shadow-none sm:px-12">
+      <Link href="/" className="flex items-center gap-2">
+        <Image
+          src="/images/site-logo.svg"
+          width={23}
+          height={23}
+          alt="BrightBase Logo"
+        />
+
+        <p className="h2-bold font-space-grotesk text-dark-100 dark:text-light-900 max-sm:hidden">
+          Bright<span className="text-primary-500">Base</span>
+        </p>
+      </Link>
+
+      <p>Global Search</p>
+
+      <div className="flex-between">
+        <Theme />
+      </div>
+    </nav>
+  );
+};
+
+export default Navbar;
+//
